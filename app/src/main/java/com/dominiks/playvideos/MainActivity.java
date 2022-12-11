c66e2061629326cd15e26816ac5ba9ee02ca0788
Dominik <dominik.stefanik@gmail.com>
2022-12-11 14:46:51 +0000

    video retrieved from catalog

--- app/src/main/java/com/dominiks/playvideos/MainActivity.java
+++ app/src/main/java/com/dominiks/playvideos/MainActivity.java
@@ -3,6 +3,9 @@ package com.dominiks.playvideos;
 import android.os.Bundle;
 
 import com.brightcove.player.analytics.Analytics;
+import com.brightcove.player.edge.Catalog;
+import com.brightcove.player.edge.VideoListener;
+import com.brightcove.player.event.EventEmitter;
 import com.brightcove.player.model.DeliveryType;
 import com.brightcove.player.model.Video;
 import com.brightcove.player.view.BrightcoveExoPlayerVideoView;
@@ -15,22 +18,25 @@ public class MainActivity extends BrightcovePlayer {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setContentView(R.layout.activity_main);
+
         brightcoveVideoView = (BrightcoveExoPlayerVideoView) findViewById(R.id.brightcove_video_view);
         super.onCreate(savedInstanceState);
 
-        Analytics analytics = brightcoveVideoView.getAnalytics();
-        analytics.setAccount("6136710637001");
+        EventEmitter eventEmitter = brightcoveVideoView.getEventEmitter();
+        String account = getString(R.string.account);
+        Catalog catalog = new Catalog.Builder(eventEmitter, account)
+                .setBaseURL(Catalog.DEFAULT_EDGE_BASE_URL)
+                .setPolicy(getString(R.string.policy))
+                .build();
 
-        Video video = Video.createVideo("https://sdks.support.brightcove.com/assets/videos/hls/greatblueheron/greatblueheron.m3u8", DeliveryType.HLS);
-        try {
-            java.net.URI myposterImage = new java.net.URI("https://sdks.support.brightcove.com/assets/images/general/Great-Blue-Heron.png");
-            video.getProperties().put(Video.Fields.STILL_IMAGE_URI, myposterImage);
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
-        }
-        // Add video
-        brightcoveVideoView.add(video);
-        // Start playback
-        brightcoveVideoView.start();
+        catalog.findVideoByID(getString(R.string.videoId), new VideoListener() {
+            @Override
+            public void onVideo(Video video) {
+                brightcoveVideoView.add(video);
+                brightcoveVideoView.start();
+            }
+        });
+
+
     }
 }
